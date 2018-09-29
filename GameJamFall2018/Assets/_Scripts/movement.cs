50fc4899c827e2d924d610aae8b251d8e8c8299b
Jessica Wei <>
2018-09-29 12:21:39 -0700

    edited movement script

--- GameJamFall2018/Assets/_Scripts/movement.cs
+++ GameJamFall2018/Assets/_Scripts/movement.cs
@@ -6,37 +6,41 @@ public class movement : MonoBehaviour {
 
     public float move = 5f; 
     public float jump = 8f;
-    float hor = .5f;
-    public Rigidbody2D myRigidBody;
+    //float hor = .5f;
+    public Rigidbody2D player;
     bool inAir;
 
     // Use this for initialization
     void Start () {
         inAir = false;
+        player = GetComponent("Rigidbody2D") as Rigidbody2D;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Horizontal Movement
-        float hInput = Input.GetAxis("Horizontal");//horizontal input from the user
-        //float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
         transform.position = transform.position + new Vector3(hInput * move * Time.deltaTime, 0, 0);
-        float hor = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(hor * move * Time.deltaTime, 0, 0);
+        //float hor = Input.GetAxis("Horizontal");
+       //transform.position = transform.position + new Vector3(hor * move * Time.deltaTime, 0, 0);
 
         //Jump
         if (Input.GetButtonDown("Jump") && inAir == false)
         {
-            myRigidBody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            //JumpDrag();
+            player.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
             inAir = true;
             Debug.Log("inAir set to true");
         }
 
     }
+
+    //Checks for collision on a platform to reset the jump bool
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.tag == "Platform" && (inAir == true)){
             inAir = false;
             Debug.Log("inAirset to false");
         }
     }
+
 }
