c0c4e66789488753b8d094154c1adc216a9666ae
Jessica Wei <>
2018-09-29 10:56:51 -0700

    put the camera on the box and initiated the respawn script

---
+++ GameJamFall2018/Assets/_Scripts/respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour {
+
+    public GameObject spawnPosition;
+    public float verticalPos = -7.35f;
+    public float horizontalPos = -2.93f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
