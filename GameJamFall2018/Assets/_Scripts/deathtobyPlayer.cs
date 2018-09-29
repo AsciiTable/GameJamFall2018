2ae63c8e18bbb2c179497f91d804bbae010fd5ea
jusin-PC\Just <justincheung6@gmail.com>
2018-09-29 11:11:43 -0700

    no message

---
+++ GameJamFall2018/Assets/_Scripts/deathtobyPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathtobyPlayer : MonoBehaviour {
+
+    void OnCollisionEnter (Collision death)
+    {
+        if (death.gameObject.tag == "Player")
+        {
+            Destroy(death.gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
