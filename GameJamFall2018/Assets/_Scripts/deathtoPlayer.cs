7653376666ac4f4e267346af23a59ec292c6c181
jusin-PC\Just <justincheung6@gmail.com>
2018-09-29 11:54:01 -0700

    Player can die (bare bones version)

---
+++ GameJamFall2018/Assets/_Scripts/deathtoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathtoPlayer : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }   
+    }
+}
