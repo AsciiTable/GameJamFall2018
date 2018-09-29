538154e40929fc7332435d79b8a0e0ba726af816
jusin-PC\Just <justincheung6@gmail.com>
2018-09-29 13:57:59 -0700

    Changed destroy to respawn, changed kill script names, added a space in respawn

--- GameJamFall2018/Assets/_Scripts/playerCollides.cs
+++ GameJamFall2018/Assets/_Scripts/playerCollides.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class deathbyPlayer : MonoBehaviour
+public class playerCollides : MonoBehaviour
 {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Deathable"))
         {
-            Destroy(collision.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
